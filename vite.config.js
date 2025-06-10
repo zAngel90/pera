2c4d3b6d875c841b5fb520e85e8780dba6176842
zAngel90 <zangel90@gmail.com>
2025-06-09 19:18:48 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
